a811e2affbffe65b9409f761a6a1976013dcefc6
Carloss0002 <carloseduardofreitasamori@gmail.com>
2022-04-05 22:28:19 -0300

    SpaceX

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center text-center">
+          <h2>SpaceX Timeline</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+   
+    <div v-if="launches.length < 0">
+      Loading...
+    </div>
+    </v-app-bar>
+    <div>
+     <v-content>
+       <v-container>
+         <v-timeline v-if="launches.length > 0">
+           <TimeLine v-for="launch in launches" :key="launch.flight_number" :launch="launch">
+
+           </TimeLine>
+         </v-timeline>
+       </v-container>
+     </v-content>
+    </div>
+    
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import TimeLine from './components/LaunchTime.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    TimeLine
+  },
+  data: () => ({
+    launches:[]
+  }),
+  async created(){
+    const {data} = await axios.get('https://api.spacexdata.com/v3/launches')
+    data.forEach(launch =>{
+      this.launches.push(launch)
+    });
+    console.log(this.launches)
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
